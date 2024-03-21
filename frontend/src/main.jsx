d65b18772f9cc87c897316acfda59bf5311c32ee
s.wyss <stefan.arnold.wyss@gmail.com>
2024-03-21 19:21:05 +0100

    feat (frontend.navbar): add two items

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <NavigationMenu>
+          <NavigationMenuList>
+              <NavigationMenuItem>
+                  <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                      <NavigationMenuLink>Link1</NavigationMenuLink>
+                  </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                  <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                      <NavigationMenuLink>Link2</NavigationMenuLink>
+                  </NavigationMenuContent>
+              </NavigationMenuItem>
+          </NavigationMenuList>
+      </NavigationMenu>
+      <App />
   </React.StrictMode>
 );
