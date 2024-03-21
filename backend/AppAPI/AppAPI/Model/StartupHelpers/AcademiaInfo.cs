62c96eba8e88da7bd0b743c25f542246bc39b00e
s.wyss <stefan.arnold.wyss@gmail.com>
2024-03-21 10:13:26 +0100

    feat (bacend): authentification routs

--- backend/AppAPI/AppAPI/Model/StartupHelpers/AcademiaInfo.cs
+++ backend/AppAPI/AppAPI/Model/StartupHelpers/AcademiaInfo.cs
@@ -2,5 +2,22 @@ namespace AppAPI.Model.StartupHelpers;
 
 public class AcademiaInfo
 {
-    
+    public AcademiaInfo(string university, string degree, string fieldOfStudy, string graduationDate, string uniStartDate, string universityCountry, string universityCity)
+    {
+        University = university;
+        Degree = degree;
+        FieldOfStudy = fieldOfStudy;
+        GraduationDate = graduationDate;
+        UniStartDate = uniStartDate;
+        UniversityCountry = universityCountry;
+        UniversityCity = universityCity;
+    }
+
+    public string University { get; set; }
+    public string Degree { get; set; }
+    public string FieldOfStudy { get; set; }
+    public string GraduationDate { get; set; }
+    public string UniStartDate { get; set; }
+    public string UniversityCountry { get; set; }
+    public string UniversityCity { get; set; }
 }
--- backend/AppAPI/AppAPI/Model/StartupHelpers/AdditionalInfo.cs
+++ backend/AppAPI/AppAPI/Model/StartupHelpers/AdditionalInfo.cs
@@ -2,5 +2,16 @@ namespace AppAPI.Model.StartupHelpers;
 
 public class AdditionalInfo
 {
-    
+    public AdditionalInfo(string englishProTest, string englishProLevel, string englishProofPath, string hearAboutStart)
+    {
+        EnglishProTest = englishProTest;
+        EnglishProLevel = englishProLevel;
+        EnglishProofPath = englishProofPath;
+        HearAboutStart = hearAboutStart;
+    }
+
+    public string EnglishProTest { get; set; }
+    public string EnglishProLevel { get; set; }
+    public string EnglishProofPath { get; set; }
+    public string HearAboutStart { get; set; }
 }
